a900a010d3efdc07b7dd5b39f69a284a3edca1f4
Karol Ulanowski <ulanowskikarol@gmail.com>
2021-01-05 17:50:06 +0100

    fixed h

--- our.py
+++ our.py
@@ -7,7 +7,7 @@ from sklearn.datasets import make_classification
 from visualize2d import plot_decision_boundary
 from scipy.optimize import minimize
 
-RANDOM_STATE = 5
+RANDOM_STATE = 0
 
 class LogisticRegression(object):
     def __init__(self, learning_rate=0.01, max_iter=1200, regularization='l2', C = 1, tolerance = 1e-4, method="CG"):
@@ -70,7 +70,7 @@ class LogisticRegression(object):
 
         res = 1/m* ((X.T @ errors) + self.lam * np.sum(theta))
 
-        return -res
+        return res
 
     def predict_proba(self, X):
         return self.h(X @ self.theta)
@@ -79,14 +79,14 @@ class LogisticRegression(object):
         return np.round(self.predict_proba(X))
 
     def h(self, z):
-        return 1 / (1 + np.exp(z))
+        return 1 / (1 + np.exp(-z))
 
 
 from sklearn.datasets import make_classification
 X, y = make_classification(n_samples=500, n_features=2, n_informative=2, n_redundant=0, n_repeated=0, n_classes=2,n_clusters_per_class=1,random_state=RANDOM_STATE)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-clf_our = LogisticRegression().fit(X_train, y_train)
+clf_our = LogisticRegression(method="CG").fit(X_train, y_train)
 print("Accuracy: " + str(accuracy_score(y_test, clf_our.predict(X_test))))
 plot_decision_boundary(X_test, y_test, clf_our)
 
